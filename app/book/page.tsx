8cb58ae887929e721de92061acb61ad716c29eb3
klyxxx <yeswork2711@gmail.com>
2026-08-31 22:45:38 +0200

    refactor(client): route legacy booking to recommendations

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
+// KLYX_LEGACY_BOOK_COMPATIBILITY_ROUTE
 export default function LegacyBookPage() {
-  redirect("/babysitters");
+  redirect("/recommendations?service=babysitting");
 }
